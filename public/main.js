09db15808087688886c384f08893209cf2da70f2
Elizabeth Hale <lizzhale@mac.com>
2015-10-11 09:42:25 -0700

    Add comments

--- public/main.js
+++ public/main.js
@@ -1,11 +1,14 @@
 $(document).ready(function() {
+    // Select the input tag and messages div
     var input = $('input');
     var messages = $('#messages');
 
+    // Create a function to append new messages
     var addMessage = function(message) {
         messages.append('<div>' + message + '</div>');
     };
 
+    // When the user presses the enter key in the input box call the addMessage function
     input.on('keydown', function(event) {
         if (event.keyCode != 13) {
             return;
